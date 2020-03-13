35d45871d28aef411931f1c00578bc920e04e506
trmigor <tarajmovich.i@phystech.edu>
2020-03-13 23:14:17 +0300

    Spin_lock init func added

--- spin_lock/main.cpp
+++ spin_lock/main.cpp
@@ -2,11 +2,14 @@
 
 typedef long s_lock;
 
+extern "C" s_lock* spin_init(s_lock *lock);
 extern "C" void spin_lock(s_lock *lock);
 extern "C" void spin_unlock(s_lock *lock);
 
 int main(void) {
-    s_lock a = 0;
+    s_lock a;
+    std::cout << a << std::endl;
+    spin_init(&a);
     std::cout << a << std::endl;
     spin_lock(&a);
     std::cout << a << std::endl;
