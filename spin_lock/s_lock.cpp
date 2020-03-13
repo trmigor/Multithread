492a8edcc7c5ffa2a8e617fd3d17229433c722e3
trmigor <tarajmovich.i@phystech.edu>
2020-03-14 00:09:17 +0300

    Added spin_delete

--- spin_lock/s_lock.cpp
+++ spin_lock/s_lock.cpp
@@ -4,3 +4,7 @@
 s_lock* spin_init(void) {
     return (s_lock*) malloc(sizeof(s_lock));
 }
+
+void spin_delete(s_lock *lock) {
+    free(lock);
+}
--- spin_lock/s_lock.hpp
+++ spin_lock/s_lock.hpp
@@ -4,6 +4,7 @@
 typedef int64_t s_lock;
 
 s_lock* spin_init(void);
+void spin_delete(s_lock *lock);
 
 extern "C" {
     void spin_lock(s_lock *lock);
